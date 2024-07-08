acdceac1e7900da8001d60b420912c2cd65c5858
Luka Gomzi <gomzi.luka77@gmail.com>
2024-07-08 15:06:48 +0200

    prepared migrations on the project

---
+++ src/migrations/1720443615062-CreateTags.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateTags1720443615062 implements MigrationInterface {
+    name = 'CreateTags1720443615062'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "tags" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, CONSTRAINT "PK_e7dc17249a1148a1970748eda99" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "tags"`);
+    }
+
+}
