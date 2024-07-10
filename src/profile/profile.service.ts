6fc2943451ccc7860344567e66d57b6c946d2123
Luka Gomzi <gomzi.luka77@gmail.com>
2024-07-10 13:29:58 +0200

    added ability to follow profile

--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -4,12 +4,15 @@ import { ProfileResponseInterface } from "@app/profile/types/profileResponse.int
 import { InjectRepository } from "@nestjs/typeorm";
 import { UserEntity } from "@app/user/user.entity";
 import { Repository } from "typeorm";
+import { FollowEntity } from "@app/profile/follow.entity";
 
 @Injectable()
 export class ProfileService {
     constructor(
         @InjectRepository(UserEntity)
-        private readonly userRepository: Repository<UserEntity>
+        private readonly userRepository: Repository<UserEntity>,
+        @InjectRepository(FollowEntity)
+        private readonly followRepository: Repository<FollowEntity>
     ) {}
 
     async getProfile(userId: number, profileUsername: string): Promise<ProfileType> {
@@ -24,6 +27,38 @@ export class ProfileService {
         }
 
         return { ...user, following: false };
+    }
+
+    async followProfile(userId: number, profileUsername: string): Promise<ProfileType> {
+        const user = await this.userRepository.findOne({
+            where: {
+                username: profileUsername
+            }
+        });
+
+        if (!user) {
+            throw new HttpException('Profile does not exist', HttpStatus.NOT_FOUND);
+        }
+
+        if (userId === user.id) {
+            throw new HttpException('Follower and Following cant be equal', HttpStatus.BAD_REQUEST);
+        }
+
+        const follow = this.followRepository.findOne({
+            where: {
+                follower: userId,
+                followingId: user.id
+            }
+        });
+
+        if (!follow) {
+            const followToCreate = new FollowEntity();
+            followToCreate.follower = userId;
+            followToCreate.followingId = user.id;
+            await this.followRepository.save(followToCreate);
+        }
+
+        return { ...user, following: true };
     }
 
     buildProfileResponse(profile: ProfileType): ProfileResponseInterface {
